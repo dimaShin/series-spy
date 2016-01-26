5f211a3f838fb806bfe22c458081b0c450e59d37
iashin <iashin.d@nixsolutions.com>
2016-01-26 18:56:10 +0200

    custom scss +

--- public/app/components/app/app-component-ctrl.js
+++ public/app/components/app/app-component-ctrl.js
@@ -1,55 +1,56 @@
+"use strict";
+
 import angular from 'angular';
 import API from './../../API/api';
 
-angular.module('components.app.ctrl', [API])
-  .controller('AppController', AppCtrl);
+class AppCtrl {
+  static $inject = ['$injector'];
 
-AppCtrl.$inject = ['$injector'];
-function AppCtrl ($injector) {
-  "use strict";
+  constructor ($injector) {
+    this.API = $injector.get('API');
+    this.$mdDialog = $injector.get('$mdDialog');
+    this.$mdToast = $injector.get('$mdToast');
 
-  this.API = $injector.get('API');
-  this.$mdDialog = $injector.get('$mdDialog');
-  this.$mdToast = $injector.get('$mdToast');
+    this.rules = this.API.rules.query();
+  }
 
-  this.rules = this.API.rules.query();
+  onDelete ($event, rule) {
+    const confirm = this.$mdDialog.confirm()
+      .title('Are you sure you want to delete ' + rule.title)
+      .textContent('All information will be lost.')
+      .targetEvent($event)
+      .ok('Delete!')
+      .cancel('Cancel');
+
+    this.$mdDialog.show(confirm).then(this._delete.bind(this, rule));
+  }
+
+  _delete (rule) {
+    rule.$delete().then(() => {
+      this.$mdToast.show(
+        this.$mdToast.simple()
+          .textContent('Success!')
+          .position('right top')
+          .parent(window.body)
+          .hideDelay(1000)
+      );
+
+      this.rules.splice(this.rules.indexOf(rule), 1);
+
+    }).catch(err => {
+      this.$mdToast.show(
+        this.$mdToast.simple()
+          .textContent(err.data)
+          .position('right top')
+          .parent(window.body)
+          .hideDelay(5000)
+      );
+    });
+  }
 }
 
-AppCtrl.prototype.deleteRule = function ($event, rule) {
-  "use strict";
-  const confirm = this.$mdDialog.confirm()
-    .title('Are you sure you want to delete ' + rule.title)
-    .textContent('All information will be lost.')
-    .targetEvent($event)
-    .ok('Delete!')
-    .cancel('Cancel');
-
-  this.$mdDialog.show(confirm).then(this._deleteRule.bind(this, rule));
-};
-
-AppCtrl.prototype._deleteRule = function (rule) {
-  "use strict";
-  rule.$delete().then(() => {
-    this.$mdToast.show(
-      this.$mdToast.simple()
-        .textContent('Success!')
-        .position('right top')
-        .parent(window.body)
-        .hideDelay(1000)
-    );
-
-    this.rules.splice(this.rules.indexOf(rule), 1);
-
-  }).catch(err => {
-    this.$mdToast.show(
-      this.$mdToast.simple()
-        .textContent(err.data)
-        .position('right top')
-        .parent(window.body)
-        .hideDelay(5000)
-    );
-  });
-};
+angular.module('components.app.ctrl', [API])
+  .controller('AppController', AppCtrl);
 
 export default 'components.app.ctrl';
 export const controller = 'AppController';
