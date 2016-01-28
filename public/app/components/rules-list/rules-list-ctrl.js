4d27cc78a305a7d01082b43d21fb9a0bef001235
iashin <iashin.d@nixsolutions.com>
2016-01-28 11:53:23 +0200

    wip

--- public/app/components/rules-list/rules-list-ctrl.js
+++ public/app/components/rules-list/rules-list-ctrl.js
@@ -10,7 +10,7 @@ class RulesList {
   constructor ($injector) {
     this.API = $injector.get('API');
     this.$mdDialog = $injector.get('$mdDialog');
-    this.$mdToast = $injector.get('$compile');
+    this.$mdToast = $injector.get('$mdToast');
     this.$rootScope = $injector.get('$rootScope');
 
     this.rules = this.API.rules.query();
@@ -37,6 +37,7 @@ class RulesList {
   startParsing () {
     const promise = this.API.parse();
     this.parsing.status = 'IN_PROGRESS';
+
     promise.then(response => {
       this.parsing.status = false;
 
@@ -49,14 +50,17 @@ class RulesList {
     });
 
     promise.catch(err => {
+
       this.parsing.status = false;
-      this.$mdToast.show(
-        this.$mdToast.simple()
-          .textContent(err.data)
-          .position('right top')
-          .parent(window.body)
-          .hideDelay(5000)
-      );
+      const toast = this.$mdToast.simple()
+        .textContent(err.data)
+        .position('right top')
+        .parent(window.body)
+        .theme('error-toast')
+        .hideDelay(5000);
+
+      this.$mdToast.show(toast);
+
     })
 
   }
