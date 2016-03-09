6b3161e4224553dbe16f8fb70dbc09defe3a3680
iashin <iashin.d@nixsolutions.com>
2016-03-09 12:53:40 +0200

    switch to ui-router

--- public/app/config/routes.js
+++ public/app/config/routes.js
@@ -1,21 +1,42 @@
 import ng from 'angular';
-import router from 'angular-route';
+import router from 'angular-ui-router';
 
 export default ng.module('app.config.router', [ router ])
-  .config(($routeProvider, $locationProvider) => {
+  .config(($stateProvider, $locationProvider, $urlRouterProvider) => {
     'ngInject';
 
     $locationProvider.html5Mode(true);
 
-    $routeProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/shows');
 
-    $routeProvider.when('/', {
-      template: '<spy-home></spy-home>',
-      resolve: {
-        user: ['user', user => {
-          "use strict";
-          return user.resolve();
-        }]
-      }
+    $stateProvider
+      .state('home',
+        {
+          url: '/',
+          abstract: true,
+          template: '<spy-home></spy-home>',
+          resolve: {
+            user: ['user', user => {
+              "use strict";
+              return user.resolve();
+            }]
+          }
+      })
+    .state('shows',
+      {
+        url: 'shows',
+        parent: 'home',
+        template: '<rules-list rules="$ctrl.user.rules"></rules-list>',
+        controller: function (user) {
+          "ngInject";
+          this.user = user;
+        },
+        controllerAs: '$ctrl'
+    })
+    .state('schedule',
+      {
+        url: 'schedule',
+        parent: 'home',
+        template: '<schedule></schedule>'
     });
   }).name;
