4c9bb1768041544b1eb4f491f83c41cf9c151dbb
iashin <iashin.d@nixsolutions.com>
2016-03-29 16:44:25 +0300

    facebook login button

--- public/app/components/sign-in/sign-in-controller.js
+++ public/app/components/sign-in/sign-in-controller.js
@@ -6,6 +6,15 @@ export default class SignInController {
     this.$state = $injector.get('$state');
     this.provider = $injector.get('provider');
     this.user = $injector.get('user');
+    this.ezfb = $injector.get('ezfb');
+
+    this.getLoginStatus();
+  }
+
+  getLoginStatus() {
+    return this.ezfb.getLoginStatus(status => {
+      console.log('fb login status: ', status);
+    });
   }
 
   anonymous() {
@@ -21,5 +30,18 @@ export default class SignInController {
       this.provider.set('local');
       this.$state.go('shows');
     });
+  }
+
+  login(provider) {
+    switch (provider) {
+      case 'fb':
+        return this._fbLogin();
+    }
+  }
+
+  _fbLogin() {
+    this.ezfb.login().then(response => {
+      console.log(response);
+    })
   }
 }
--- public/app/run.js
+++ public/app/run.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
 
 export default angular.module('app.run', [])
-  .run(function($rootScope, $state) {
+  .run(function($rootScope, $state, ezfb) {
     'ngInject'
-
+    ezfb.init({
+      appId      : '1162828690396646',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
     $rootScope.$on('$stateChangeError', ($ev, to, toParams, from, fromParams, err) => {
       $state.go('signIn');
 
